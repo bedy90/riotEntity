9dc4d8f1b3e2fcfe7e063908f802e11a78c27226
MBelangerB <47693085+MBelangerB@users.noreply.github.com>
2024-06-08 11:08:27 -0400

    GenerateEntities now a class

--- src/interface/Account/index.ts
+++ src/interface/Account/index.ts
@@ -1,7 +1,12 @@
-import * as AccountDTOInterface from './v1/IAccountDTO';
+// Namespace version
+import * as AccountInterfaces from './v1/IAccountDTO';
+// import { IAccountDTO } from './v1/IAccountDTO';
+// import { IActiveShardDTO } from './v1/IActiveShardDTO';
 
 export namespace Account {
-  export namespace v1 {
-    export import IAccountDTO = AccountDTOInterface.IAccountDTO;
-  }
+    export namespace v1 {
+        export import IAccountDTO = AccountInterfaces.IAccountDTO;
+
+        // export default { AccountInterfaces.IAccountDTO, AccountInterfaces.IActiveShardDTO } as const;
+    }
 }
