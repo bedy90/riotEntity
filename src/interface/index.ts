4471e8aaaa552aaf16e17ea4daaec5c352e2793f
MBelangerB <47693085+MBelangerB@users.noreply.github.com>
2024-06-09 16:27:03 -0400

    Fix global Index interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -48,15 +48,36 @@ export * from './LOL_Challenges/v1/IChallengeConfigInfoDTO';
 // @NotImplemented
 // export * from './LOL_Challenges/v1/IPlayerInfoDTO';
 
-export * from './LOL_Status/v4/IStatus';
+export * from './LOL_Status/v4/IContentDTO';
+export * from './LOL_Status/v4/IPlatformDataDTO';
+export * from './LOL_Status/v4/IStatusDTO';
+export * from './LOL_Status/v4/IUpdateDTO';
 
+export * from './Match/v5/IBanDTO';
+export * from './Match/v5/IInfoDTO';
 export * from './Match/v5/IMatchDTO';
 // @NotImplemented
 // export * from './Match/v5/IMatchTimelineDTO';
+export * from './Match/v5/IMetadataDTO';
+export * from './Match/v5/IObjectiveDTO';
+export * from './Match/v5/IObjectivesDTO';
+export * from './Match/v5/IParticipantDTO';
 export * from './Match/v5/IPerksDTO';
+export * from './Match/v5/IPerkStatsDTO';
+export * from './Match/v5/IPerkStyleDTO';
+export * from './Match/v5/IPerkStyleSelectionDTO';
+export * from './Match/v5/ITeamDTO';
 
+
+export * from './Spectactor/v4/IBannedChampion';
 export * from './Spectactor/v4/ICurrentGameInfo';
+export * from './Spectactor/v4/ICurrentGameParticipant';
 export * from './Spectactor/v4/IFeaturedGames';
+export * from './Spectactor/v4/IFeaturedGameInfo';
+export * from './Spectactor/v4/IGameCustomizationObject';
+export * from './Spectactor/v4/IObserver';
+export * from './Spectactor/v4/IParticipant';
+export * from './Spectactor/v4/IPerks';
 
 export * from './Summoner/v4/ISummonerDTO';
 
@@ -64,27 +85,27 @@ export { ILeagueEntryDTO as TFT_ILeagueEntryDTO } from './TFT_League/v1/ILeagueE
 export { ILeagueListDTO as TFT_ILeagueListDTO } from './TFT_League/v1/ILeagueListDTO';
 export { IMiniSeriesDTO as TFT_IMiniSeriesDTO } from './TFT_League/v1/IMiniSeriesDTO';
 
-export { ICompanionDTO as TFT_ICompanionDTO } from './TFT_Match/v1/IMatchDTO';
-export { IInfoDTO as TFT_IInfoDTO } from './TFT_Match/v1/IMatchDTO';
+export { ICompanionDTO as TFT_ICompanionDTO } from './TFT_Match/v1/ICompanionDTO';
+export { IInfoDTO as TFT_IInfoDTO } from './TFT_Match/v1/IInfoDTO';
 export { IMatchDTO as TFT_IMatchDTO } from './TFT_Match/v1/IMatchDTO';
-export { IMetadataDTO as TFT_IMetadataDTO } from './TFT_Match/v1/IMatchDTO';
-export { IParticipantDTO as TFT_IParticipantDTO } from './TFT_Match/v1/IMatchDTO';
-export { ITraitDTO as TFT_ITraitDTO } from './TFT_Match/v1/IMatchDTO';
-export { IUnitDTO as TFT_IUnitDTO } from './TFT_Match/v1/IMatchDTO';
+export { IMetadataDTO as TFT_IMetadataDTO } from './TFT_Match/v1/IMetadataDTO';
+export { IParticipantDTO as TFT_IParticipantDTO } from './TFT_Match/v1/IParticipantDTO';
+export { ITraitDTO as TFT_ITraitDTO } from './TFT_Match/v1/ITraitDTO';
+export { IUnitDTO as TFT_IUnitDTO } from './TFT_Match/v1/IUnitDTO';
 
+export { IBannedChampion as TFT_IBannedChampion } from './TFT_Spectactor/v5/IBannedChampion';
 export { ICurrentGameInfo as TFT_ICurrentGameInfo } from './TFT_Spectactor/v5/ICurrentGameInfo';
-export { ICurrentGameParticipant as TFT_ICurrentGameParticipant } from './TFT_Spectactor/v5/ICurrentGameInfo';
-export { IGameCustomizationObject as TFT_IGameCustomizationObject } from './TFT_Spectactor/v5/ICurrentGameInfo';
-export { IPerks as TFT_IPerks } from './TFT_Spectactor/v5/ICurrentGameInfo';
-export { IBannedChampion as TFT_IBannedChampion } from './TFT_Spectactor/v5/IFeaturedGames';
-export { IFeaturedGameInfo as TFT_IFeaturedGameInfo } from './TFT_Spectactor/v5/IFeaturedGames';
+export { ICurrentGameParticipant as TFT_ICurrentGameParticipant } from './TFT_Spectactor/v5/ICurrentGameParticipant';
+export { IFeaturedGameInfo as TFT_IFeaturedGameInfo } from './TFT_Spectactor/v5/IFeaturedGameInfo';
 export { IFeaturedGames as TFT_IFeaturedGames } from './TFT_Spectactor/v5/IFeaturedGames';
-export { IObserver as TFT_IObserver } from './TFT_Spectactor/v5/IFeaturedGames';
-export { IParticipant as TFT_IParticipant } from './TFT_Spectactor/v5/IFeaturedGames';
+export { IGameCustomizationObject as TFT_IGameCustomizationObject } from './TFT_Spectactor/v5/IGameCustomizationObject';
+export { IObserver as TFT_IObserver } from './TFT_Spectactor/v5/IObserver';
+export { IParticipant as TFT_IParticipant } from './TFT_Spectactor/v5/IParticipant';
+export { IPerks as TFT_IPerks } from './TFT_Spectactor/v5/IPerks';
 
-export { IContentDTO as TFT_IContentDTO } from './TFT_Status/v1/IStatus';
-export { IPlatformDataDTO as TFT_IPlatformDataDTO } from './TFT_Status/v1/IStatus';
-export { IStatusDTO as TFT_IStatusDTO } from './TFT_Status/v1/IStatus';
-export { IUpdateDTO as TFT_IUpdateDTO } from './TFT_Status/v1/IStatus';
+export { IContentDTO as TFT_IContentDTO } from './TFT_Status/v1/IContentDTO';
+export { IPlatformDataDTO as TFT_IPlatformDataDTO } from './TFT_Status/v1/IPlatformDataDTO';
+export { IStatusDTO as TFT_IStatusDTO } from './TFT_Status/v1/IStatusDTO';
+export { IUpdateDTO as TFT_IUpdateDTO } from './TFT_Status/v1/IUpdateDTO';
 
 export { ISummonerDTO as TFT_ISummonerDTO } from './TFT_Summoner/v1/ISummonerDTO';
