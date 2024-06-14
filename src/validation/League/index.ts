a40acbdd2ab0859053d0cb6a2bd4291cf3c050ed
MBelangerB <47693085+MBelangerB@users.noreply.github.com>
2024-06-14 15:02:30 -0400

    Prepare Validation generator (not working)

--- src/validation/League/index.ts
+++ src/validation/League/index.ts
@@ -1,13 +1,16 @@
 import * as LeagueEntryCheck from './v4/LeagueEntryChecker';
 import * as LeagueListCheck from './v4/LeagueListChecker';
+import * as LeagueItemChecker from './v4/LeagueItemChecker';
+import * as MiniSeriesChecker from './v4/MiniSeriesChecker';
 // export * from './v4/PlayerInfoDTO';
 
 export namespace League {
     export namespace v4 {
         export import isILeagueEntryDTO = LeagueEntryCheck.isILeagueEntryDTO;
 
-        export import isILeagueItemDTO = LeagueListCheck.isILeagueItemDTO;
+        export import isILeagueItemDTO = LeagueItemChecker.isILeagueItemDTO;
         export import isILeagueListDTO = LeagueListCheck.isILeagueListDTO;
-        export import isILeagueEntryDisIMiniSeriesDTOTO = LeagueListCheck.isIMiniSeriesDTO;
+
+        export import isIMiniSeriesDTO = MiniSeriesChecker.isIMiniSeriesDTO;
     }
 }
