54de0fc37a42ed0a5706a77f7357cb0194c26c89
MBelangerB <47693085+MBelangerB@users.noreply.github.com>
2024-06-14 14:43:59 -0400

    Validator Premise

--- src/validation/Account/v1/AccountChecker.ts
+++ src/validation/Account/v1/AccountChecker.ts
@@ -1,6 +1,25 @@
 import { Interfaces } from '@/riotentity';
 
 export function isIAccountDTO(obj: any): obj is Interfaces.Account.v1.IAccountDTO {
-    const hasFields = 'puuid' in obj && 'gameName' in obj && 'tagLine' in obj && Object.keys(obj).length === 3;
-    return hasFields;
+    if (typeof obj !== 'object' || obj === null) {
+        return false;
+    }
+
+    // Pourrait potentiellement pré-créer un objet et ensuite comparer les « keys »
+    // let data: Interfaces.Account.v1.IAccountDTO = {gameName:'',puuid: '', tagLine: ''};
+    // Object.keys(data)
+
+    const accountDTO = obj as Interfaces.Account.v1.IAccountDTO;
+
+    const hasFieldsIn: boolean = 'puuid' in accountDTO && 'gameName' in accountDTO && 'tagLine' in accountDTO;
+
+    const hasFieldCount: boolean = Object.keys(accountDTO).length === 3;
+
+    const hasFieldType: boolean = (
+        typeof accountDTO.puuid === 'string' &&
+        typeof accountDTO.gameName === 'string' &&
+        typeof accountDTO.tagLine === 'string'
+    );
+
+    return hasFieldsIn && hasFieldCount && hasFieldType;
 }
