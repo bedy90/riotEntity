44067557cdadc80b5c5b604b2afdceed73ab073c
MBelangerB <47693085+MBelangerB@users.noreply.github.com>
2024-06-15 10:43:54 -0400

    Update version Audit fix

---
+++ scripts/template/namespace.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let headerImport: string[];
+    export let namespaceName: string;
+    export let namespaceVersion: string;
+    export let expImps: string[];
+</script>
+
+<template>
+    {#each headerImport as importPath}
+      {importPath}
+    {/each}
+  
+    export namespace {namespaceName} {{
+      export namespace {namespaceVersion} {{
+        {#each expImps as expImport}
+          {expImport}
+        {/each}
+      }} 
+    }}
+</template>
